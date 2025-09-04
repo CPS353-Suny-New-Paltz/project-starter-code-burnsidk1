01efc6407d99eaff5cf35f585c561e5d5f03e67a
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-04 16:09:59 -0400

    Renamed Checkpoint_1 to Checkpoint1 for checkstyle
    File was failing checkstyle because of the naming error

--- src/Checkpoint1.java
+++ src/Checkpoint1.java
@@ -1,4 +1,4 @@
-public class checkpoint_1 {
+public class Checkpoint1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
