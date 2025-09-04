a8f47c0569b0b6aa22d8f0e00f5a85625324a0b2
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-04 16:32:32 -0400

    checks

--- src/Checkpoint1.java
+++ src/Checkpoint1.java
@@ -1,7 +1,6 @@
 public class Checkpoint1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
         System.out.println("short line");
     }
 }
