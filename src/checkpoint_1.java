72155f8210ec1b243455e253291595bdbbb4e942
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-04 16:00:02 -0400

    Updated "checkpoint_1.java" for checks
    Added the needed semicolon and removed the very long line

--- src/checkpoint_1.java
+++ src/checkpoint_1.java
@@ -1,7 +1,7 @@
 public class checkpoint_1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!")
-        System.out.println("..................................... really long line ............................................... ");
+        System.out.println("Hello, World!");
+        System.out.println("short line");
     }
 }
