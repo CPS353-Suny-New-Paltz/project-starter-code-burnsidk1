5e7c9ed0894cb3e9d75cac028d540472f8fa561f
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-05 12:53:51 -0400

    Updated Checkpoint1.java
    Reverted back to the version with errors

--- src/Checkpoint1.java
+++ src/Checkpoint1.java
@@ -1,6 +1,11 @@
 public class Checkpoint1{
     public static void main(String[] args){
+<<<<<<< Updated upstream
         System.out.println("Hello, World!");
         System.out.println("short line");
+=======
+        System.out.println("Hello, World!")
+        System.out.println("short line......................................................");
+>>>>>>> Stashed changes
     }
 }
