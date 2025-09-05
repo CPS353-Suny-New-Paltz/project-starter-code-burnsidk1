978820a9dddb4ac2addfe24f3bce628f7ce37178
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-05 12:55:35 -0400

    Updated Checkpoint1.java
    Fixed errors

--- src/Checkpoint1.java
+++ src/Checkpoint1.java
@@ -1,11 +1,6 @@
 public class Checkpoint1{
     public static void main(String[] args){
-<<<<<<< Updated upstream
         System.out.println("Hello, World!");
         System.out.println("short line");
-=======
-        System.out.println("Hello, World!")
-        System.out.println("short line......................................................");
->>>>>>> Stashed changes
     }
 }
