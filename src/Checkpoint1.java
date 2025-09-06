6638cb26bc430ed04607648b2aeb155470f81fc8
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-06 09:36:49 -0400

    Checkpoint 1 With Errors
    Lack of semi-colon and an excessively long line

--- src/Checkpoint1.java
+++ src/Checkpoint1.java
@@ -1,6 +1,6 @@
 public class Checkpoint1{
     public static void main(String[] args){
-        System.out.println("Hello, World!");
-        System.out.println("short line");
+        System.out.println("Hello, World!")
+        System.out.println("Long line.................................................................................................");
     }
 }
