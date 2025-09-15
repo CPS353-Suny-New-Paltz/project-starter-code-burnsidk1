ccd4d1c6d4c099112451f4dbaf5f6fe211969b93
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-15 10:43:33 -0400

    Created ComputeEngine API for Checkpoint 2
    Made the initial protoype files that include no actual computations or implemented code.

---
+++ src/api/PrototypeComputeEngine.java
@@ -0,0 +1,21 @@
+package api; 
+
+import project.annotations.ConceptualAPIPrototype;
+
+public class PrototypeComputeEngine {
+	@ConceptualAPIPrototype 
+	public void prototype(ComputeEngineAPI engine) {
+		// Request wrapper
+		ComputeStartRequest startRequest = new ComputeStartRequest(); 
+		
+		// Gets a start response when trying to begin a computation
+	ComputeStartResponse startResponse = engine.startCompute(startRequest); 
+	
+	if (startResponse.getCode() == ComputeStatusCode.SUCCESS) {
+		// The computation was able to start successfully 
+		} else { 
+			// Handle errors (invalid request, unavailable, etc.
+			// that will be added later) 
+			} 
+	} 
+}
