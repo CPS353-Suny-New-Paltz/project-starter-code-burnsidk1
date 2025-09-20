938e1c3f25699e7a9b3f2afb66562ac66d37b294
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-20 10:57:25 -0400

    Added 2 API smoke tests
    Created smoke tests for ComputeEngineAPI and UserNetworkAPI. I'm still working on DataStorageAPI because it seems to be a bit more complicated. I am likely missing a few things from these tests, so I will add them if I am. I also am currently not able to run the tests because of some issue with my file paths that I have to figure out.

---
+++ test/tests/TestUserNetworkAPI.java
@@ -0,0 +1,35 @@
+package tests;
+
+import api.ComputeEngineAPI;
+import api.DataStorageAPI;
+import api.UserJobStartRequest;
+import api.UserJobStartResponse;
+import api.UserNetworkAPI;
+import impl.UserNetworkAPIImpl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class TestUserNetworkAPI {
+	
+    @Test
+    void submitJob_withCustomDelimiter_smoke() {
+        DataStorageAPI storage = mock(DataStorageAPI.class);
+        ComputeEngineAPI compute = mock(ComputeEngineAPI.class);
+        UserNetworkAPI api = new UserNetworkAPIImpl(storage, compute);
+
+        UserJobStartRequest req = new UserJobStartRequest(";");
+
+        UserJobStartResponse resp = api.submitJob(req);
+
+        assertNotNull(resp, "Smoke: submitJob returns response");
+    }
+
+    @Test
+    void request_defaultDelimiter_isComma() {
+        UserJobStartRequest req = new UserJobStartRequest();
+        assertEquals(",", req.getDelimiter());
+    }
+}
