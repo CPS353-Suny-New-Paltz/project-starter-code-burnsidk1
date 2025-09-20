257f1a0669ae255cd698f8642d8cf5c678ee8153
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-20 11:00:32 -0400

    Fixed Checkstyle Errors
    Commented out "import static org.junit.jupiter.api.Assertions.*;" because Checkstyle does not like the * and I can't import the methods until I fix my testing issues.

--- test/tests/TestUserNetworkAPI.java
+++ test/tests/TestUserNetworkAPI.java
@@ -9,7 +9,7 @@ import impl.UserNetworkAPIImpl;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class TestUserNetworkAPI {
