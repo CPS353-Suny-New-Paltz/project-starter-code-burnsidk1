0a6dfb36b6d4180fcd1b20dc38987912d4d43ed8
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-21 10:23:44 -0400

    In Memory Implementation of The Input and Output
    Added the InMemoryOutputConfig and InMemoryInputConfig classes that will be the infrastructure for integration testing of the DataStorage and UserNetwork APIs. Also added the support InputConfig and OutputConfig classes.

--- src/api/DataStorageAPI.java
+++ src/api/DataStorageAPI.java
@@ -1,11 +1,21 @@
 package api;
 
 import project.annotations.ProcessAPI;
+import java.util.List;
 
 @ProcessAPI
 public interface DataStorageAPI {
-    // Store the results
     StorageWriteResponse writeResults(StorageWriteRequest request);
+
+    // Input and output locations
     InputBatch readInputs(String inputLocation);
-    WriteResult writeOutputs(String outputLocation, java.util.List<String> formattedPairs);
+    WriteResult writeOutputs(String outputLocation, List<String> formattedPairs);
+
+    // Overload
+    default InputBatch readInputs(InputConfig cfg) {
+        return readInputs(cfg.getLocation());
+    }
+    default WriteResult writeOutputs(OutputConfig cfg, List<String> formattedPairs) {
+        return writeOutputs(cfg.getLocation(), formattedPairs);
+    }
 }
---
+++ src/api/InputConfig.java
@@ -0,0 +1,6 @@
+package api;
+
+public interface InputConfig {
+	// Describes where the inputs are
+    String getLocation();
+}
