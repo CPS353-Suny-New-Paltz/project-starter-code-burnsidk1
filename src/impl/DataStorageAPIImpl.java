79c635200c6c25ecd1acfc49180d7e0470139b1c
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-20 21:27:37 -0400

    Added Comments to Current Files

--- src/impl/DataStorageAPIImpl.java
+++ src/impl/DataStorageAPIImpl.java
@@ -12,27 +12,31 @@ import api.WriteResult;
 public class DataStorageAPIImpl implements DataStorageAPI {
 
     private final DataStore dataStore; 
-    
+    // No argument constructor for the smoke test
     public DataStorageAPIImpl() { 
     	this.dataStore = null; 
     	}
 
+    // Storage dependency
     public DataStorageAPIImpl(DataStore dataStore) {
         this.dataStore = dataStore;
     }
 
+    // Read integers from a user-defined location
 	@Override
     public InputBatch readInputs(String inputLocation) {
-        return null;
+        return null; // Returns null for now
     }
 
+	// Write outputs to destination
     @Override
     public WriteResult writeOutputs(String outputLocation, List<String> formattedPairs) {
-        return null;
+        return null; // Returns null for now
     }
 
+    // Wrapper for the request/response
     @Override
     public StorageWriteResponse writeResults(StorageWriteRequest request) {
-        return null;
+        return null; // Returns null for now
     }
 }
