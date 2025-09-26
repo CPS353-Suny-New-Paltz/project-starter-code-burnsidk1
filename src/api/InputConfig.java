5e55c7388727291a768e95fea0fdf9f8450d31d8
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-25 21:00:56 -0400

    Revert "In Memory Implementation of The Input and Output"
    This reverts commit 0a6dfb36b6d4180fcd1b20dc38987912d4d43ed8.

--- src/api/InputConfig.java
+++
@@ -1,6 +0,0 @@
-package api;
-
-public interface InputConfig {
-	// Describes where the inputs are
-    String getLocation();
-}
--- src/api/OutputConfig.java
+++
@@ -1,6 +0,0 @@
-package api;
-
-public interface OutputConfig {
-	// Where outputs are written to
-    String getLocation();
-}
