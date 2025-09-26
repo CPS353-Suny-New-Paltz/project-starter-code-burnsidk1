32052d3064c4dc8fec2f19e3cabb6941990b8fe8
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-25 20:04:35 -0400

    Comments to APIs to Make Them Appear in This PR
    Gradle can't see my annotations for some reason and that seems to be why the tests are failing. Maybe "making them visible" will help.

--- src/api/UserNetworkAPI.java
+++ src/api/UserNetworkAPI.java
@@ -1,7 +1,7 @@
 package api;
 
 import project.annotations.NetworkAPI;
-
+// Comment to see if this fixes Gradle issue from Checkpoint3
 @NetworkAPI
 public interface UserNetworkAPI {
     // The user starts a job
