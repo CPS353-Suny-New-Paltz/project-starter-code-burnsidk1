96dba44732ea84a895a1391233ceb2a84c13cace
burnsidk1 <burnsidk1@newpaltz.edu>
2025-09-30 19:03:00 -0400

    Updated ManualTestingFramework
    Put back the ToDos that were on a different pull request.

--- test/project/checkpointtests/ManualTestingFramework.java
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -1,14 +1,17 @@
 package project.checkpointtests;
 
-import edu.softwareeng.sample.ComputationCoordinator;
-import edu.softwareeng.sample.ComputeEngine;
-import edu.softwareeng.sample.ComputeEngineImpl;
-import edu.softwareeng.sample.ComputeRequest;
-import edu.softwareeng.sample.CoordinatorImpl;
-import edu.softwareeng.sample.DataStore;
-import edu.softwareeng.sample.DataStoreImpl;
-import edu.softwareeng.sample.FileInputConfig;
-import edu.softwareeng.sample.FileOutputConfig;
+import impl.DataStorageAPIImpl;
+import impl.FileDataStore;
+import impl.ComputeEngineAPIImpl; 
+import impl.UserNetworkAPIImpl;
+
+import api.DataStorageAPI;
+import api.DataStore;
+import api.ComputeEngineAPI;  
+import api.UserNetworkAPI; 
+
+import api.UserJobStartRequest;
+import api.UserJobStartResponse;
 
 public class ManualTestingFramework {
     
@@ -19,12 +22,23 @@ public class ManualTestingFramework {
         // TODO 1:
         // Instantiate a real (ie, class definition lives in the src/ folder) implementation 
         // of all 3 APIs
-        //
-        // TODO 2:
+    	DataStore fileStore = new FileDataStore();
+    	
+    	DataStorageAPI dataStore = new DataStorageAPIImpl(fileStore);
+    	ComputeEngineAPI engine = new ComputeEngineAPIImpl();
+    	UserNetworkAPI coordinator = new UserNetworkAPIImpl(dataStore, engine);
+    	
+        
+    	// TODO 2:
         // Run a computation with an input file of <root project dir>/manualTestInput.txt
         // and an output of <root project dir>/manualTestOutput.txt, with a delimiter of ',' 
-        //
-        //
+    	UserJobStartRequest request = new UserJobStartRequest(",", INPUT, OUTPUT);
+    	
+        UserJobStartResponse response = coordinator.submitJob(request);
+
+        System.out.println("Manual run with INPUT=" + INPUT +
+                           ", OUTPUT=" + OUTPUT + ", delimiter=','" +
+                           " Response=" + (response == null ? "null" : response.toString()));
         // Helpful hint: the working directory of this program is <root project dir>,
         // so you can refer to the files just using the INPUT/OUTPUT constants. You do not 
         // need to (and should not) actually create those files in your repo
