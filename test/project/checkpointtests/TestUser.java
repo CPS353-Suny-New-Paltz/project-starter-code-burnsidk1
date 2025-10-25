c84186022c00b8ec97d819d20fd3b91266a0a6a7
burnsidk1 <burnsidk1@newpaltz.edu>
2025-10-25 11:12:56 -0400

    Fixed TODOs on TestUser
    Changed the variable type to UserNetworkAPI and added the method calls for the coordinator to run a compute job.

--- test/project/checkpointtests/TestUser.java
+++ test/project/checkpointtests/TestUser.java
@@ -1,15 +1,18 @@
 package testHarness;
 
 import java.io.File;
+import api.UserNetworkAPI;
+import api.UserJobStartRequest;
+import api.UserJobStartResponse;
 
 
 public class TestUser {
 	
 	// TODO 3: change the type of this variable to the name you're using for your
 	// @NetworkAPI interface; also update the parameter passed to the constructor
-	private final ComputationCoordinator coordinator;
+	private final UserNetworkAPI coordinator;
 
-	public TestUser(ComputationCoordinator coordinator) {
+	public TestUser(UserNetworkAPI coordinator) {
 		this.coordinator = coordinator;
 	}
 
@@ -19,6 +22,8 @@ public class TestUser {
 		
 		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
 		// run the compute job specified by inputPath, outputPath, and delimiter
+		UserJobStartRequest request = new UserJobStartRequest(inputPath, outputPath, String.valueOf(delimiter));
+		UserJobStartResponse response = coordinator.submitJob(request);
 	}
 
 }
