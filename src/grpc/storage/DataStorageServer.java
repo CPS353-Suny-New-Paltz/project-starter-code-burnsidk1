d0672dd0d81bc1bdb152fcc73fe069c01f70302e
burnsidk1 <burnsidk1@newpaltz.edu>
2025-11-04 12:14:31 -0500

    Created DataStorageServer
    Created the server file for DataStorage that has the gRPC server.

---
+++ src/grpc/storage/DataStorageServer.java
@@ -0,0 +1,29 @@
+package grpc.storage;
+
+import api.DataStorageAPI;
+import impl.DataStorageAPIImpl;
+import impl.FileDataStore;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class DataStorageServer {
+    public static void main(String[] args) throws Exception {
+        // Initialize the DataStorageAPI implementation
+        DataStorageAPI storage = new DataStorageAPIImpl(new FileDataStore());
+        // Start the gRPC server
+        Server server = ServerBuilder.forPort(50052)
+                .addService(new DataStorageServiceImpl(storage))
+                .build()
+                .start();
+        System.out.println("DataStorage gRPC server started on port 50052");
+        // Shutting down
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("DataStorageServer shutting down.");
+            // Shutdown the gRPC server
+            server.shutdown();
+            System.out.println("DataStorageServer stopped.");
+        }));
+        // Keep the server running until termination
+        server.awaitTermination();
+    }
+}
